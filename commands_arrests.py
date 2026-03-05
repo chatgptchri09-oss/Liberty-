a71b069e95d0cb4c3b65a89c50c0ff5733f2ab64
chatgptchri09-oss <chatgptchri09@gmail.com>
2026-03-05 18:06:15 +0100

    Update LOG_CHANNEL_ID and SCERIFFO_ROLES values

--- commands_arrests.py
+++ commands_arrests.py
@@ -3,8 +3,8 @@ from discord import app_commands
 import database
 from datetime import datetime
 
-LOG_CHANNEL_ID = 1415297578022604850
-SCERIFFO_ROLES = [1415093546549248040]
+LOG_CHANNEL_ID = 1479158931610931414
+SCERIFFO_ROLES = 1404051875426467902
 
 def has_sceriffo(interaction: discord.Interaction) -> bool:
     if not isinstance(interaction.user, discord.Member):
